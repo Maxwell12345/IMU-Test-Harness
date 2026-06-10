7a1730450de5e7556275490537256f8d691c0946
TheBrandTheDon <tranbrandon15@gmail.com>
2026-06-10 13:42:23 -0500

    WIP

--- src/gps/GpsManager.cpp
+++ src/gps/GpsManager.cpp
@@ -1,6 +1,6 @@
 /******************************************************************************
  * File:             GpsManager.cpp
- * 
+ *
  * Author:           Atkinson Maj Brian R.
  * Organization:     Marine Corps Software Factory
  * Created On:       6/10/26 9:47 AM
@@ -9,33 +9,57 @@
 #include "GpsManager.hpp"
 
 void GpsManager::start() {
-    this->m_serialPort(this->m_ioContext, this->m_comPort);
-    this->m_serialPort.set_option(boost::asio::serial_port_base::baud_rate(this->m_baudRate));
+  this->m_serialPort.emplace(this->m_ioContext, this->m_comPort);
 
-    this->m_readThread = std::jthread([this]() {
-        
-    });
+  this->m_serialPort.set_option(boost::asio::serial_port_base::baud_rate(this->m_baudRate));
+  this->m_serialPort.set_option(boost::asio::serial_port_base::character_size(8));
+  this->m_serialPort.set_option(boost::asio::serial_port_base::stop_bits(boost::asio::serial_port_base::stop_bits::one));
+  this->m_serialPort.set_option(boost::asio::serial_port_base::parity(boost::asio::serial_port_base::parity::none));
+  this->m_serialPort.set_option(boost::asio::serial_port_base::flow_control(boost::asio::serial_port_base::flow_control::none));
 
-    this->m_running.store(true);
+  this->m_running.store(true);
+
+  this->m_readThread = std::jthread([this](std::stop_token st) {
+    this->readLoop(st);
+    // TODO: set running to false
+  });
 }
 
-void GpsManager::stop() {
-}
+void GpsManager::stop() {}
 
-bool GpsManager::isRunning() const {
-}
+bool GpsManager::isRunning() const {}
 
-GpsManagerStats GpsManager::getStats() const {
-}
+GpsManagerStats GpsManager::getStats() const {}
 
 void GpsManager::readLoop(std::stop_token stopToken) {
-}
+  std::stop_callback onStop(stopToken, [this]() {
+    boost::system::error_code ec;
+    if (this->m_serialPort && this->m_serialPort->is_open()) {
+      this->m_serialPort->cancel(ec);
+    }
+  });
 
-std::optional<IMUUtils::GpsUpdate> GpsManager::parseNmea(const std::string &sentence) {
-}
+  while (!stopToken.stop_requested()) {
+    boost::system::error_code ec;
+    boost::asio::read_until(*this->m_serialPort, this->m_readBuf, '\n', ec);
+    if (ec)
+      break;
 
-bool GpsManager::validateFix(const IMUUtils::GpsUpdate &update) const {
-}
+    std::istream serialStream(&this->m_readBuf);
+    std::string sentence;
+    std::getline(stream, sentence);
+    if (!sentence.empty() && sentence.back() == '\r') {
+      sentence.pop_back();
+      {
+        std::lock_guard(this->m_queueMutex);
+        this->m_sentenceQueue.push(std::move(sentence));
+      }
+      this->m_queueCv.notify_one();
+    }
+  }
 
-void GpsManager::publishUpdate(IMUUtils::GpsUpdate update) {
-}
+  std::optional<IMUUtils::GpsUpdate> GpsManager::parseNmea(const std::string &sentence) {}
+
+  bool GpsManager::validateFix(const IMUUtils::GpsUpdate &update) const {}
+
+  void GpsManager::publishUpdate(IMUUtils::GpsUpdate update) {}
--- src/gps/GpsManager.hpp
+++ src/gps/GpsManager.hpp
@@ -1,6 +1,6 @@
 /******************************************************************************
  * File:             GpsManager.hpp
- * 
+ *
  * Author:           Atkinson Maj Brian R.
  * Organization:     Marine Corps Software Factory
  * Created On:       6/10/26 9:47 AM
@@ -11,91 +11,85 @@
 #ifndef INU_DISPLAY_GPSMANAGER_HPP
 #define INU_DISPLAY_GPSMANAGER_HPP
 
+#include <boost/asio.hpp>
+#include <boost/shared_ptr.hpp>
 #include <string>
 #include <thread>
-#include <boost/shared_ptr.hpp>
-#include <boost/asio.hpp>
 
 #include "utils.hpp"
 
 class DatabaseManager;
 
 struct GpsManagerStats {
-    std::atomic<uint64_t> totalSentencesReceived{0};
-    std::atomic<uint64_t> acceptedFixes{0};
-    std::atomic<uint64_t> parseFailures{0};
-    std::atomic<uint64_t> checksumFailures{0};
-    std::atomic<uint64_t> validationRejected{0};
-    std::atomic<uint64_t> dbEnqueueFailures{0};
+  std::atomic<uint64_t> totalSentencesReceived{0};
+  std::atomic<uint64_t> acceptedFixes{0};
+  std::atomic<uint64_t> parseFailures{0};
+  std::atomic<uint64_t> checksumFailures{0};
+  std::atomic<uint64_t> validationRejected{0};
+  std::atomic<uint64_t> dbEnqueueFailures{0};
 };
 
 class GpsManager {
 public:
-    GpsManager(
-        const std::string& comPort,
-        unsigned int baudRate
-    ) {
-        if (comPort.empty()) {
-            throw std::invalid_argument("GpsManager: specified com port cannot be empty");
-        }
-        if (baudRate == 0) {
-            throw std::invalid_argument("GpsManager: specified baud rate cannot be zero");
-        }
-        this->m_comPort = comPort;
-        this->m_baudRate = baudRate;
-    };
+  GpsManager(const std::string &comPort, unsigned int baudRate) {
+    if (comPort.empty()) {
+      throw std::invalid_argument("GpsManager: specified com port cannot be empty");
+    }
+    if (baudRate == 0) {
+      throw std::invalid_argument("GpsManager: specified baud rate cannot be zero");
+    }
+    this->m_comPort = comPort;
+    this->m_baudRate = baudRate;
+  };
 
-    GpsManager(
-        const std::string& comPort,
-        unsigned int baudRate,
-        boost::shared_ptr<DatabaseManager> databaseManager
-    ) {
-        if (comPort.empty()) {
-            throw std::invalid_argument("GpsManager: specified com port cannot be empty");
-        }
-        if (baudRate == 0) {
-            throw std::invalid_argument("GpsManager: specified baud rate cannot be zero");
-        }
-        if (databaseManager == nullptr) {
-            throw std::invalid_argument("GpsManager: specified database manager cannot be null");
-        }
-        this->m_comPort = comPort;
-        this->m_baudRate = baudRate;
-        this->m_databaseManager = std::move(databaseManager);
-    };
+  GpsManager(const std::string &comPort, unsigned int baudRate, boost::shared_ptr<DatabaseManager> databaseManager) {
+    if (comPort.empty()) {
+      throw std::invalid_argument("GpsManager: specified com port cannot be empty");
+    }
+    if (baudRate == 0) {
+      throw std::invalid_argument("GpsManager: specified baud rate cannot be zero");
+    }
+    if (databaseManager == nullptr) {
+      throw std::invalid_argument("GpsManager: specified database manager cannot be null");
+    }
+    this->m_comPort = comPort;
+    this->m_baudRate = baudRate;
+    this->m_databaseManager = std::move(databaseManager);
+  };
 
-    ~GpsManager() = default;
+  ~GpsManager() = default;
 
-    void start();
+  void start();
 
-    void stop();
+  void stop();
 
-    bool isRunning() const;
+  bool isRunning() const;
 
-    GpsManagerStats getStats() const;
+  GpsManagerStats getStats() const;
 
 private:
-    void readLoop(std::stop_token stopToken);
+  void readLoop(std::stop_token stopToken);
 
-    std::optional<IMUUtils::GpsUpdate> parseNmea(const std::string& sentence);
+  std::optional<IMUUtils::GpsUpdate> parseNmea(const std::string &sentence);
 
-    bool validateFix(const IMUUtils::GpsUpdate& update) const;
+  bool validateFix(const IMUUtils::GpsUpdate &update) const;
 
-    void publishUpdate(IMUUtils::GpsUpdate update);
-
+  void publishUpdate(IMUUtils::GpsUpdate update);
 
 private:
-    std::string m_comPort;
-    boost::asio::serial_port m_serialPort;
-    unsigned int m_baudRate;
-    boost::shared_ptr<DatabaseManager> m_databaseManager;
-    boost::asio::io_context m_ioContext;
-    std::jthread m_readThread;
-    std::atomic<bool> m_running;
-    mutable std::mutex m_statsMutex;
-    GpsManagerStats m_stats;
-
+  std::string m_comPort;
+  boost::asio::serial_port m_serialPort;
+  unsigned int m_baudRate;
+  boost::shared_ptr<DatabaseManager> m_databaseManager;
+  std::optional<boost::asio::io_context> m_ioContext;
+  boost::asio::streambuf m_readBuf;
+  std::queue<std::string> m_sentenceQueue;
+  std::jthread m_readThread;
+  std::atomic<bool> m_running;
+  std::mutex m_statsMutex;
+  std::mutex m_queueMutex;
+  std::condition_variable m_queueCv;
+  GpsManagerStats m_stats;
 };
 
-
-#endif //INU_DISPLAY_GPSMANAGER_HPP
+#endif // INU_DISPLAY_GPSMANAGER_HPP
