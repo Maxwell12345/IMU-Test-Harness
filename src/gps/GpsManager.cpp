e8fdb75b12ec1d3f2a7553377740026f9124c2e5
Maxwell <idlerm3@gmail.com>
2026-06-11 09:23:12 -0500

    Add GPS Manager

--- src/gps/GpsManager.cpp
+++ src/gps/GpsManager.cpp
@@ -6,148 +6,75 @@
  * Created On:       6/10/26 9:47 AM
  *
  ******************************************************************************/
-#include "GpsManager.hpp"
-#include "GpsUpdate.hpp"
 
-void GpsManager::start() {
-    this->m_serialPort.emplace(this->m_ioContext, this->m_comPort);
+ #include "GpsManager.hpp"
 
-    this->m_serialPort.set_option(boost::asio::serial_port_base::baud_rate(this->m_baudRate));
-    this->m_serialPort.set_option(boost::asio::serial_port_base::character_size(8));
-    this->m_serialPort.set_option(
-        boost::asio::serial_port_base::stop_bits(boost::asio::serial_port_base::stop_bits::one));
-    this->m_serialPort.set_option(boost::asio::serial_port_base::parity(boost::asio::serial_port_base::parity::none));
-    this->m_serialPort.set_option(
-        boost::asio::serial_port_base::flow_control(boost::asio::serial_port_base::flow_control::none));
+#define NMEA_PORT "/dev/ttyACM0"
 
-    this->m_running.store(true);
+GpsManager::GpsManager()
+    : m_nmeaReader(NMEA_PORT, 9600) {}
 
-    this->m_readThread = std::jthread([this](std::stop_token st) {
-        this->readLoop(st);
-        // TODO: set running to false
-    });
-
-    this->m_queueThread = std::jthread([this](std::stop_token st) {
-    });
+void GpsManager::InstallCallback(std::function<void(const GpsUpdate&)> callback) {
+    std::lock_guard<std::mutex> lock(m_callbackMutex);
+    m_callback = callback;
+    m_isCallbackInstalled = true;
 }
 
-void GpsManager::stop() {
-}
+void GpsManager::Start() {
+    if (m_running.exchange(true)) return;
 
-bool GpsManager::isRunning() const {
-    return this->m_running.load();
-}
-
-GpsManagerStats GpsManager::getStats() const {
-}
-
-void GpsManager::readLoop(std::stop_token stopToken) {
-    std::stop_callback onStop(stopToken, [this]() {
-        boost::system::error_code ec;
-        if (this->m_serialPort && this->m_serialPort->is_open()) {
-            this->m_serialPort->cancel(ec);
-        }
-    });
-
-    while (!stopToken.stop_requested()) {
-        boost::system::error_code ec;
-        boost::asio::read_until(*this->m_serialPort, this->m_readBuf, '\n', ec);
-        if (ec)
-            break;
-
-        std::istream serialStream(&this->m_readBuf);
-        std::string sentence;
-        std::getline(stream, sentence);
-        if (!sentence.empty() && sentence.back() == '\r') {
-            sentence.pop_back(); {
-                std::lock_guard<std::mutex> queueLock(this->m_queueMutex);
-                this->m_sentenceQueue.push(std::move(sentence));
-            }
-            this->m_queueCv.notify_one();
-        }
+    if (!m_nmeaReader.open()) {
+        m_running = false;
+        return;
     }
-}
 
-void GpsManager::processQueue() {
-    while (this->m_running.load()) {
-        try {
-            std::string serialSentence;
+    m_thread = std::thread([this]() {
+        NmeaMessage msg;
+
+        while (m_running) {
+            if (!m_nmeaReader.readMessage(msg)) continue;
+            if (!msg.validChecksum) continue;
+
+            GpsUpdate update = BuildGpsUpdate(msg);
+
+            std::function<void(const GpsUpdate&)> callback;
+
             {
-                std::unique_lock<std::mutex> queueLock(this->m_queueMutex);
-                this->m_queueCv.wait(queueLock, [this]() { return !this->m_sentenceQueue.empty(); });
-                serialSentence = this->m_sentenceQueue.front();
-                this->m_sentenceQueue.pop();
+                std::lock_guard<std::mutex> lock(m_callbackMutex);
+                if (!m_isCallbackInstalled) continue;
+                callback = m_callback;
             }
 
-
-            NMEAParserData::ERROR_E e = m_nmeaParser.ProcessNMEABuffer(serialSentence.c_str(), serialSentence.size());
-
-            if (e != NMEAParserData::ERROR_E::ERROR_OK) {
-                std::cout << "Not a NMEA payload!\n";
-            }
-
-            if (serialSentence.find("GGA") != std::string::npos) {
-                NMEASentenceGGA gga = m_nmeaParser.GetGGA();
-
-                NMEAParserData::GGA_DATA_T data = gga.GetSentenceData();
-
-                auto now = std::chrono::steady_clock::now();
-                auto ms_since_epoch = std::chrono::duration_cast<std::chrono::milliseconds>(
-                    now.time_since_epoch()
-                ).count();
-
-                // TODO: Not sure what all the time fields mean.
-                GpsUpdate updatedGps = {
-                    now,
-                    now,
-                    data.m_dLatitude,
-                    data.m_dLongitude,
-                    std::nullopt,
-                    data.m_nGPSQuality,
-                    data.m_nSatsInView,
-                    data.m_dHDOP,
-                    ms_since_epoch,
-                    true
-                };
-
-                this->m_gpsUpdateCallback(updatedGps);
-            }
-            else if (serialSentence.find("RMC") != std::string::npos) {
-                NMEASentenceRMC rmc = m_nmeaParser.GetRMC();
-
-                NMEAParserData::RMC_DATA_T data = rmc.GetSentenceData();
-
-                auto now = std::chrono::steady_clock::now();
-                auto ms_since_epoch = std::chrono::duration_cast<std::chrono::milliseconds>(
-                    now.time_since_epoch()
-                ).count();
-
-                // TODO: Not sure what all the time fields mean.
-                GpsUpdate updatedGps = {
-                    now,
-                    now,
-                    data.m_dLatitude,
-                    data.m_dLongitude,
-                    data.m_dTrackAngle,
-                    0,
-                    0,
-                    0,
-                    ms_since_epoch,
-                    true
-                };
-
-                this->m_gpsUpdateCallback(updatedGps);
-            }
+            callback(update);
         }
-        catch (std::exception &e) {
-            std::cerr << e.what() << std::endl;
-        }
+    });
+}
+
+void GpsManager::Stop() {
+    if (!m_running.exchange(false)) return;
+
+    m_nmeaReader.close();
+
+    if (m_thread.joinable()) {
+        m_thread.join();
     }
 }
 
-bool GpsManager::validateFix(const IMUUtils::GpsUpdate &update) const {
-    
-}
+GpsUpdate GpsManager::BuildGpsUpdate(const NmeaMessage& msg) {
+    GpsUpdate update;
 
-void GpsManager::publishUpdate(IMUUtils::GpsUpdate update) {
+    update.receiveTime = std::chrono::steady_clock::now();
+    update.wallTime = std::chrono::system_clock::now();
+    update.latitude = msg.lat;
+    update.longitude = msg.lon;
+    update.fixQuality = static_cast<uint8_t>(msg.fixQuality);
+    update.numSatellites = static_cast<uint8_t>(msg.numSatellites);
+    update.hdop = msg.hdop;
+    update.valid = msg.validChecksum && msg.validFix;
+
+    if (msg.type == "GPRMC" || msg.type == "GNRMC") {
+        update.heading = msg.courseDeg;
+    }
+
+    return update;
 }
--- src/gps/GpsManager.hpp
+++ src/gps/GpsManager.hpp
@@ -11,109 +11,33 @@
 #ifndef INU_DISPLAY_GPSMANAGER_HPP
 #define INU_DISPLAY_GPSMANAGER_HPP
 
-#include <condition_variable>
+#include "NmeaReader.hpp"
+#include "GpsUpdate.hpp"
+
 #include <functional>
-#include <boost/asio.hpp>
-#include <boost/shared_ptr.hpp>
-#include <queue>
-#include <string>
 #include <thread>
-
-#include "utils.hpp"
-// #include "NMEAParserWrapper.hpp"
-#include "NMEAParser.hpp"
-
-class DatabaseManager;
-class GpsUpdate;
-
-struct GpsManagerStats {
-  std::atomic<uint64_t> totalSentencesReceived{0};
-  std::atomic<uint64_t> acceptedFixes{0};
-  std::atomic<uint64_t> parseFailures{0};
-  std::atomic<uint64_t> checksumFailures{0};
-  std::atomic<uint64_t> validationRejected{0};
-  std::atomic<uint64_t> dbEnqueueFailures{0};
-};
+#include <atomic>
+#include <mutex>
 
 class GpsManager {
 public:
-  GpsManager(const std::string &comPort,
-    unsigned int baudRate,
-    std::function<void(const GpsUpdate&)> gpsUpdateCallback):m_gpsUpdateCallback(gpsUpdateCallback) {
-    if (comPort.empty()) {
-      throw std::invalid_argument("GpsManager: specified com port cannot be empty");
-    }
-    if (baudRate == 0) {
-      throw std::invalid_argument("GpsManager: specified baud rate cannot be zero");
-    }
-    this->m_comPort = comPort;
-    this->m_baudRate = baudRate;
-  };
+    GpsManager();
 
-  GpsManager(const std::string &comPort,
-    unsigned int baudRate,
-    boost::shared_ptr<DatabaseManager> databaseManager,
-    std::function<void(const GpsUpdate&)> gpsUpdateCallback):m_gpsUpdateCallback(gpsUpdateCallback) {
-    if (comPort.empty()) {
-      throw std::invalid_argument("GpsManager: specified com port cannot be empty");
-    }
-    if (baudRate == 0) {
-      throw std::invalid_argument("GpsManager: specified baud rate cannot be zero");
-    }
-    if (databaseManager == nullptr) {
-      throw std::invalid_argument("GpsManager: specified database manager cannot be null");
-    }
-    this->m_comPort = comPort;
-    this->m_baudRate = baudRate;
-    this->m_databaseManager = std::move(databaseManager);
-  };
-
-  ~GpsManager() = default;
-
-  void start();
-
-  void stop();
-
-  bool isRunning() const;
-
-  GpsManagerStats getStats() const;
+    void InstallCallback(std::function<void(const GpsUpdate&)> callback);
+    void Start();
+    void Stop();
 
 private:
-  void readLoop(std::stop_token stopToken);
+    GpsUpdate BuildGpsUpdate(const NmeaMessage& msg);
 
-  void processQueue();
+    std::function<void(const GpsUpdate&)> m_callback;
+    std::mutex m_callbackMutex;
+    bool m_isCallbackInstalled = false;
 
-  bool validateFix(const IMUUtils::GpsUpdate &update) const;
+    std::atomic<bool> m_running = false;
+    std::thread m_thread;
 
-  void publishUpdate(IMUUtils::GpsUpdate update);
-
-private:
-  std::atomic<bool> m_running;
-
-  std::optional<boost::asio::io_context> m_ioContext;
-  std::string m_comPort;
-  boost::asio::serial_port m_serialPort;
-  unsigned int m_baudRate;
-
-  // read data from serial port into some buffer for future processing
-  std::jthread m_readThread;
-  boost::asio::streambuf m_readBuf;
-
-  // clear data from serial/read buffer, process into parsed NMEA data, send updates
-  std::jthread m_queueThread;
-  std::mutex m_queueMutex;
-  std::condition_variable m_queueCv;
-  std::queue<std::string> m_sentenceQueue;
-
-  NMEAParser m_nmeaParser;
-
-  std::function<void(const GpsUpdate&)> m_gpsUpdateCallback;
-
-  std::mutex m_statsMutex;
-  GpsManagerStats m_stats;
-
-  boost::shared_ptr<DatabaseManager> m_databaseManager;
-
+    NmeaReader m_nmeaReader;
 };
 
 #endif // INU_DISPLAY_GPSMANAGER_HPP
