4586aeb91d0f024706349ceff169565d58c2b72d
Brian Atkinson <brian.r.atkinson@usmc.mil>
2026-06-22 16:24:58 -0500

    Implemented working serial writer for linear acceleration

--- microcontroller/main/main.c
+++ microcontroller/main/main.c
@@ -22,12 +22,38 @@ void app_main(void) {
 
     ESP_ERROR_CHECK(host_serial_init());
 
-    acceleration_t acceleration = {
-        1.0f,
-        2.0f,
-        3.0f,
-        0x445566778899AABB
-    };
+    while (true) {
+        acceleration_t acceleration = {
+            1.0f,
+            2.0f,
+            3.0f,
+            56
+        };
 
-    ESP_ERROR_CHECK(send_acceleration_t(&acceleration));
+        // 0x00 00 80 3f
+        // 0x00 00 00 40
+        // 0x00 00 40 40,
+        // 0x00 00 00 00 00 00 00 38
+
+        send_acceleration_t(&acceleration);
+
+        // uint8_t buffer[ACCELERATION_MSG_BYTES] = {
+        //     0xff, 0x01, 0x14,
+        //     0x00, 0x00, 0x80, 0x3f,
+        //     0x00, 0x00, 0x00, 0x40,
+        //     0x00, 0x00, 0x40, 0x40,
+        //     0xbb, 0xaa, 0x99, 0x88, 0x77, 0x66, 0x55, 0x44,
+        //     0x0a, 0x20
+        // };
+
+        // ESP_ERROR_CHECK(host_serial_write_all(buffer, ACCELERATION_MSG_BYTES));
+
+
+
+        // printf("ff00140000803f0000004000004040bbaa99887766554467f8");
+        // printf("%s", buffer);
+        // ESP_ERROR_CHECK(send_acceleration_t(&acceleration));
+
+        vTaskDelay(pdMS_TO_TICKS(500));
+    }
 }
--- microcontroller/main/serial.h
+++ microcontroller/main/serial.h
@@ -23,6 +23,28 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
+#define HOST_UART        UART_NUM_0
+#define HOST_UART_BAUD   115200
+
+#define RX_BUFFER_SIZE   512
+#define TX_BUFFER_SIZE   512
+
+// magic (1B) + type (1B) + payloadLen (1B) + payload (N B) + checksum (2B)
+#define ACCELERATION_PAYLOAD_BYTES (3*sizeof(float)+sizeof(uint64_t))
+#define ACCELERATION_MSG_BYTES (1+1+1+ACCELERATION_PAYLOAD_BYTES+2)
+#define ROTATION_PAYLOAD_BYTES (5*sizeof(float)+sizeof(uint64_t)+1)
+#define ROTATION_MSG_BYTES (1+1+1+ROTATION_PAYLOAD_BYTES+2)
+
+#define ACCELERATION_T_ID 1
+#define ROTATION_T_ID 2
+#define ERROR_MSG_ID 3
+
+#define MAGIC_BYTE_IDX 0
+#define MSG_TYPE_IDX 1
+#define PAYLOAD_LEN_IDX 2
+#define PAYLOAD_START_IDX 3
+#define ACCELERATION_CRC_IDX (PAYLOAD_START_IDX+ACCELERATION_PAYLOAD_BYTES)
+
 typedef struct acceleration_t {
     float x;
     float y;
