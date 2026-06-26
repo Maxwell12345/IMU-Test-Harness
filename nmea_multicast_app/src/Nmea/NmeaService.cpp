77bfb5d83ecea72b642dc79ecd7909a74c0d9dbc
Darrick <darkeyegraphics@gmail.com>
2026-06-26 16:28:22 -0500

    done

--- nmea_multicast_app/src/Nmea/NmeaService.cpp
+++ nmea_multicast_app/src/Nmea/NmeaService.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string>
+#include <boost/regex.hpp>
 
 #include "NmeaService.hpp"
 
@@ -42,6 +44,35 @@ void NmeaService::Stop() {
 
 void NmeaService::Callback(SerialPortBase& port) {
     std::lock_guard nmeaGuard(m_nmeaMutex);
-    port.ReadUntil(m_nmea, "\n");
+
+    // NMEA Standard 0183 sentence ends with CRLF
+    port.ReadUntil(m_nmea, "\r\n");
+
+    if(!IsValidNmea(m_nmea)) {
+        return;
+    }
+    if(!IsValidChecksum(m_nmea)) {
+        return;
+    }
     std::cout << "[DEBUG] " << m_multicastService->Send(m_nmea) << " bytes sent\n";
+}
+
+bool NmeaService::IsValidChecksum(const std::string& nmea) {
+    char checksum = nmea[1];
+    for(size_t i = 2; i < nmea.size(); i++) {
+        if(nmea[i] == '*') {
+            break;
+        }
+        checksum ^= nmea[i];
+    }
+
+    int realChecksum = std::stoi(nmea.substr(nmea.length() - 2, 2), nullptr, 16);
+
+    return realChecksum == checksum;
+}
+
+bool NmeaService::IsValidNmea(const std::string& nmea) {
+    std::string patternStr = R"(^\$([A-Z]{2}[A-Z0-9]{1,5}),([^*]*)\*([0-9A-Fa-f]{2})$)";
+    const boost::regex pattern(patternStr, boost::regex::icase);
+    return boost::regex_match(nmea, pattern);
 }
--- nmea_multicast_app/src/Nmea/NmeaService.hpp
+++ nmea_multicast_app/src/Nmea/NmeaService.hpp
@@ -6,6 +6,7 @@
 #include <thread>
 #include <atomic>
 #include <mutex>
+#include <gtest/gtest_prod.h> 
 
 class SerialPortBase;
 class SerialComService;
@@ -39,6 +40,25 @@ public:
     void Stop();
 
 private:
+
+    /**
+     * @brief XOR characters between $ and * exclusively and compare to the checksum included
+     *      in the payload
+     * 
+     * @param [in] nmea the nmea sentence without CRLF
+     * 
+     * @return true if paylaod checksum matches the calculated checksum, else false
+     */
+    static bool IsValidChecksum(const std::string& nmea);
+    
+    /**
+     * @brief nmea string validity check
+     * 
+     * @param [in] nmea string ot be checked
+     * 
+     * @return true if valid, else false
+     */
+    static bool IsValidNmea(const std::string& nmea);
     
     /**
      * @brief On com read handler
@@ -58,6 +78,11 @@ private:
 
     std::unique_ptr<SerialComService> m_serialComService;       //used to read from serial com port
     std::unique_ptr<MulticastService> m_multicastService;       //used to multicast m_nmea sentence upon successful com read
+
+    FRIEND_TEST(NmeaServiceTest, IsValidNmeaTestReturnsTrue);
+    FRIEND_TEST(NmeaServiceTest, IsValidNmeaTestReturnsFalse);
+    FRIEND_TEST(NmeaServiceTest, IsValidChecksumReturnsTrue);
+    FRIEND_TEST(NmeaServiceTest, IsValidChecksumReturnsFalse);
 };
 
 #endif
